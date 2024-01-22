b4a0efe9fe5c70c6fe66c73e6e346429d37b5339
Sanieeme <shoneesani@gmail.com>
2024-01-22 03:58:22 -0800

    s

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm
+ * @array: pararmeter
+ * @size: size
+ * Return: array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k, n;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		j = i;
+		for (k = i + 1; k < size; k++)
+		{
+			if (array[k] < array[j])
+				j = k;
+		}
+		if (j != i)
+		{
+			n = array[j];
+			array[j] = array[i];
+			array[i] = n;
+			print_array(array, size);
+		}
+	}
+}
