ed856d0f048b81a287e1ba9cfe81269f190290a5
Sanieeme <shoneesani@gmail.com>
2024-01-23 06:18:42 -0800

    s

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,11 +8,14 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, n;
+	int s;
 
 	if (array == NULL || size < 2)
 		return;
+
 	for (i = 0; i < size; i++)
 	{
+		s = 0;
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -20,8 +23,11 @@ void bubble_sort(int *array, size_t size)
 				n = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = n;
+				s++;
 				print_array(array, size);
 			}
 		}
+		if (s == 0)
+			return;
 	}
 }
